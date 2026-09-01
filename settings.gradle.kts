26823c3b0ce88b7d73d3396feedaa5207393cdec
ozdil <29512292+ozdil@users.noreply.github.com>
2026-09-01 16:09:37 +0300

    fix: configure google and mavenCentral plugin repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "omasend-android"
 include(":app")
